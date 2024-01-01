f3c3b6037fd4aab65ce034252855597c8635c4d1
Fazley Rabbi <69764331+Fazley-Rabbi06@users.noreply.github.com>
2024-01-01 17:41:28 +0600

    Time: 17 ms (47.73%), Space: 45.8 MB (9.37%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -7,19 +7,13 @@ class Solution {
         for (Character key : s.toCharArray())
             freqMap.put(key, freqMap.getOrDefault(key, 0) + 1);
         //sort the map in des order
-        freqMap = freqMap.entrySet()
-                .stream()
-                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (e1, e2) -> e1,
-                        LinkedHashMap::new
-                ));
+        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> freqMap.get(b) - freqMap.get(a));
+        maxHeap.addAll(freqMap.keySet());
 
-        for (Map.Entry<Character, Integer> eachData : freqMap.entrySet()) {
-            for (int i = 0; i < eachData.getValue(); i++) {
-                res.append(eachData.getKey());
+      while (!maxHeap.isEmpty()){
+          Character eachChar = maxHeap.remove();
+            for (int i = 0; i < freqMap.get(eachChar); i++) {
+                res.append(eachChar);
             }
         }
         return res.toString();
